cd3c84dab7bb30d3b706e740fa510e726b6e2b59
Chris Richards <chris.richards@yellowfeather.co.uk>
2021-07-19 16:42:05 +0100

    Adds GiftVoucher.Category

--- src/Basket/GiftVoucher.cs
+++ src/Basket/GiftVoucher.cs
@@ -2,13 +2,15 @@
 {
     public class GiftVoucher
     {
-        public GiftVoucher(string code, decimal value)
+        public GiftVoucher(string code, decimal value, ProductCategory? category = null)
         {
             Code = code;
             Value = value;
+            Category = category;
         }
 
         public string Code { get; private set; }
         public decimal Value { get; private set; }
+        public ProductCategory? Category { get; }
     }
 }
